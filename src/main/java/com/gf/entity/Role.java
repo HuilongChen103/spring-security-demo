8f8650b7052d4b84ed1c3384443b99ee1a6b3e45
chl <1422437585@qq.com>
2020-05-15 17:28:18 +0800

    添加spring security demo

---
+++ src/main/java/com/gf/entity/Role.java
@@ -0,0 +1,34 @@
+package com.gf.entity;
+
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+
+@Data
+public class Role implements GrantedAuthority {
+
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getAuthority() {
+        return name;
+    }
+
+}
---
+++ src/main/java/com/gf/entity/RolePermisson.java
@@ -0,0 +1,28 @@
+package com.gf.entity;
+
+import lombok.Data;
+
+@Data
+public class RolePermisson {
+
+    private String url;
+    private String roleName;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+
+}
